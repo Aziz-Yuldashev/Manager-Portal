0f78c88e02c2f62080480ca4cf432920ae1fe204
Aziz <azizuldashev560@gmail.com>
2025-04-05 05:50:48 +0500

    refactoring code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,34 +9,26 @@ import Deposit from '@/components/pagesTabs/deposit/index'
 import Installment from '@/components/pagesTabs/installment/index'
 import Messages from '@/components/pagesTabs/messages/index'
 
+const TABS = {
+    shop: Shop,
+    bills: Bills,
+    deposit: Deposit,
+    credit: Credit,
+    installment: Installment,
+    messages: Messages,
+}
+
 export default function Home() {
     const { activeTab } = useTab()
+    const ActiveComponent = TABS[activeTab] ?? null
 
-    const renderTab = () => {
-        switch (activeTab) {
-            case 'shop':
-                return <Shop />
-            case 'bills':
-                return <Bills />
-            case 'deposit':
-                return <Deposit />
-            case 'credit':
-                return <Credit />
-            case 'installment':
-                return <Installment />
-            case 'messages':
-                return <Messages />
-            default:
-                return null
-        }
-    }
     return (
         <div>
             <Header />
             <Sidebar />
             <div style={{ display: 'flex', gap: '40px' }}>
                 <SidebarMenu />
-                {renderTab()}
+                {ActiveComponent && <ActiveComponent />}
             </div>
         </div>
     )
